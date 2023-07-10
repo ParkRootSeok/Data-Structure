21864893997a860238c9b18ce5f2b17e46d1ea35
parkrootseok <parkrootseok@gmail.com>
2023-07-10 18:30:31 +0900

    [Add] 연산 횟수 비교를 위한 코드

--- 23-1/linearsearch/LinearSearch.c
+++ 23-1/linearsearch/LinearSearch.c
@@ -3,9 +3,12 @@
 /* 
   * 순차 탐색
   * 시간 복잡도 : T[n] = n (찾은 원소가 n-1번 째에 있을 경우 최악의 경우로 n번 실행)
+  * 데이터 수 증가에 따라 연산횟수 증가가 비례
 */
 
 int linearSearch(int * arr, int len, int target) {
+    
+    int cnt = 0;
 
     for (int i = 0; i < len; i++) {
         
@@ -13,7 +16,9 @@ int linearSearch(int * arr, int len, int target) {
             return i; // 찾은 대상의 인덱스를 반환
         }
 
-    }
+        cnt++;    
+    } printf("연산 횟수 : %d\n", cnt);
+    
     return -1;
 
 }
@@ -26,14 +31,14 @@ int main(void) {
         
         int idx, target;
 
-        printf("target = ");
+        printf("Target = ");
         scanf("%d", &target);
         idx = linearSearch(arr, sizeof(arr) / sizeof(int), target);
 
         if (idx == -1) {
             printf("Fail to Search\n");
         } else {
-            printf("Target index : %d\n", idx);
+            printf("Target Index : %d\n", idx);
         }
         
     }
